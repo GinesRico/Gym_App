2df0292a4ca501d2decc98a19e1551bdd3bf6a76
GinesRico <ginesrico89@gmail.com>
2024-05-21 19:08:40 +0200

    cambios a la hora de llamar a la bd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import flet as ft
 import sqlite3
-import os 
+import os
 
 def main(page: ft.Page):
     page.title = "Ejercicios"
@@ -8,16 +8,15 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     # Ruta de la imagen
-    image_base_path = "/Users/ginesrico/Desktop/gym_app/assets"
-    
+    image_base_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets"
+    bd_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets/datos_ejercicios.db"
 
     # Conectar a la base de datos y crear la tabla de favoritos si no existe
-    conn = sqlite3.connect('datos_ejercicios.db')
+    conn = sqlite3.connect(bd_path)
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS favoritos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE)")
     cursor.execute("SELECT DISTINCT bodyPart FROM datos")
     body_parts = cursor.fetchall()
-    conn.close()
 
     history = []
 
@@ -28,14 +27,11 @@ def main(page: ft.Page):
         eq = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
         page.add(eq)
 
-        conn = sqlite3.connect('datos_ejercicios.db')
-        cursor = conn.cursor()
         cursor.execute("SELECT DISTINCT equipment FROM datos WHERE bodyPart=?", (body_part,))
         equipments = cursor.fetchall()
-        conn.close()
 
         for equip in equipments:
-            image_path = os.path.join(image_base_path, f"{body_part[0].lower()}.png")
+            image_path = os.path.join(image_base_path, f"{body_part.lower()}.png")
             button = ft.ElevatedButton(
                 content=ft.Container(
                     content=ft.Column(
@@ -49,47 +45,33 @@ def main(page: ft.Page):
                 ),
                 on_click=lambda e: exercise_screen(e, body_part),
                 data=equip[0],
-                style=ft.ButtonStyle(
-                    shape=ft.RoundedRectangleBorder(radius=5)
-                )
+                style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5))
             )
             eq.controls.append(button)
-        
+
         page.update()
 
     def exercise_screen(e, body_part):
         equipment = e.control.data
         history.append("equipment")
         page.clean()
-        conn = sqlite3.connect('datos_ejercicios.db')
-        cursor = conn.cursor()
+
         cursor.execute("SELECT name, gifUrl, instructions FROM datos WHERE bodyPart=? AND equipment=?", (body_part, equipment))
         ejercicios = cursor.fetchall()
-        conn.close()
 
-        ejercicio_card = ft.Column(
-            spacing=10,
-            expand=True,
-            scroll=ft.ScrollMode.ALWAYS
-        )
+        ejercicio_card = ft.Column(spacing=10, expand=True, scroll=ft.ScrollMode.ALWAYS)
 
-        # Función para abrir el diálogo correspondiente al ejercicio
         def open_dlg(instructions):
             dlg = ft.AlertDialog(
                 title=ft.Text("Instrucciones"),
                 content=ft.Text(instructions),
-                actions=[
-                    ft.TextButton("Cerrar", on_click=lambda _: page.dialog.close())
-                ]
+                actions=[ft.TextButton("Cerrar", on_click=lambda _: page.dialog.close())]
             )
             page.dialog = dlg
             dlg.open = True
             page.update()
 
-        # Función para alternar el estado de favorito
         def mark_favorite(name):
-            conn = sqlite3.connect('datos_ejercicios.db')
-            cursor = conn.cursor()
             cursor.execute("SELECT name FROM favoritos WHERE name=?", (name,))
             favorito = cursor.fetchone()
             if favorito:
@@ -100,25 +82,19 @@ def main(page: ft.Page):
                 cursor.execute("INSERT INTO favoritos (name) VALUES (?)", (name,))
                 conn.commit()
                 message = f"{name.title()} añadido a favoritos"
-            conn.close()
+
             page.snack_bar = ft.SnackBar(content=ft.Text(message))
             page.snack_bar.open = True
             page.update()
-            exercise_screen(e, body_part)  # Refresh the screen to update the favorite icon
+            exercise_screen(e, body_part)
 
         for ejercicio in ejercicios:
             name, gifUrl, instructions = ejercicio
 
-            # Verificar si el ejercicio es un favorito
-            conn = sqlite3.connect('datos_ejercicios.db')
-            cursor = conn.cursor()
             cursor.execute("SELECT name FROM favoritos WHERE name=?", (name,))
             favorito = cursor.fetchone()
-            conn.close()
-
             favorite_icon = ft.icons.STAR if favorito else ft.icons.STAR_OUTLINE_OUTLINED
 
-            # Crear contenido de tarjeta para el ejercicio actual
             card_content = ft.Card(
                 content=ft.Container(
                     content=ft.Column(
@@ -138,63 +114,43 @@ def main(page: ft.Page):
                                     )
                                 ],
                                 alignment=ft.MainAxisAlignment.END,
-                            ),                            
+                            ),
                         ]
                     ),
                     padding=10,
                 )
             )
-            
             ejercicio_card.controls.append(card_content)
 
         page.add(ejercicio_card)
 
     def favorites_screen():
         page.clean()
-        conn = sqlite3.connect('datos_ejercicios.db')
-        cursor = conn.cursor()
         cursor.execute("SELECT name FROM favoritos")
         favoritos = cursor.fetchall()
-        conn.close()
 
-        favorites_list = ft.Column(
-            spacing=10,
-            expand=True,
-            scroll=ft.ScrollMode.ALWAYS
-        )
-
-
+        favorites_list = ft.Column(spacing=10, expand=True, scroll=ft.ScrollMode.ALWAYS)
 
         for fav in favoritos:
             name = fav[0]
-            conn = sqlite3.connect('datos_ejercicios.db')
-            cursor = conn.cursor()
             cursor.execute("SELECT gifUrl, instructions FROM datos WHERE name=?", (name,))
             ejercicio = cursor.fetchone()
-            conn.close()
             if ejercicio:
                 gifUrl, instructions = ejercicio
 
-                # Función para abrir el diálogo correspondiente al ejercicio
                 def open_dlg(instructions):
                     dlg = ft.AlertDialog(
                         title=ft.Text("Instrucciones"),
                         content=ft.Text(instructions.capitalize()),
-                        actions=[
-                            ft.TextButton("Cerrar", on_click=lambda _: page.dialog.close())
-                        ]
+                        actions=[ft.TextButton("Cerrar", on_click=lambda _: page.dialog.close())]
                     )
                     page.dialog = dlg
                     dlg.open = True
                     page.update()
 
-                # Función para eliminar de favoritos
                 def remove_favorite(name):
-                    conn = sqlite3.connect('datos_ejercicios.db')
-                    cursor = conn.cursor()
                     cursor.execute("DELETE FROM favoritos WHERE name=?", (name,))
                     conn.commit()
-                    conn.close()
                     page.snack_bar = ft.SnackBar(content=ft.Text(f"{name.title()} eliminado de favoritos"))
                     page.snack_bar.open = True
                     page.update()
@@ -239,7 +195,7 @@ def main(page: ft.Page):
 
     def home():
         page.add(
-            ft.Image(src=f"icon.png", height=400),
+            ft.Image(src="icon.png", height=400),
             ft.Container(padding=20),
             ft.Text("Bienvenido!"),
             ft.Container(padding=20),
@@ -250,11 +206,8 @@ def main(page: ft.Page):
         bp = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
         page.add(bp)
 
-        conn = sqlite3.connect('datos_ejercicios.db')
-        cursor = conn.cursor()
         cursor.execute("SELECT DISTINCT bodyPart FROM datos")
         body_parts = cursor.fetchall()
-        conn.close()
 
         for body_part in body_parts:
             image_path = os.path.join(image_base_path, f"{body_part[0].lower()}.png")
@@ -271,13 +224,11 @@ def main(page: ft.Page):
                 ),
                 on_click=equipment_screen,
                 data=body_part[0],
-                style=ft.ButtonStyle(
-                    shape=ft.RoundedRectangleBorder(radius=5)
-                )
+                style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5))
             )
             bp.controls.append(button)
         page.update()
-        
+
     def nav_change(e):
         selected_index = e.control.selected_index
         if selected_index == 0:
@@ -305,12 +256,13 @@ def main(page: ft.Page):
         ]
     )
     page.add(
-        ft.Image(src=f"icon.png", height=400),
+        ft.Image(src="icon.png", height=400),
         ft.Container(padding=20),
         ft.Text("Bienvenido!"),
         ft.Container(padding=20),
         ft.Container(padding=20),
     )
 
-ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER)
+    home()
 
+ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER)
