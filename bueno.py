48457e7e5ef6c8ba98b42b712dd78f7bb5528bda
GinesRico <ginesrico89@gmail.com>
2024-05-22 19:24:35 +0200

    cambios

--- bueno.py
+++ bueno.py
@@ -4,7 +4,7 @@ import os
 
 # Ruta de la imagen
 image_base_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets"
-bd_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets/ejercicios.db"
+bd_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets/datos_ejercicios.db"
 
 # Conectar a la base de datos y crear tablas si no existen
 def initialize_database():
@@ -31,7 +31,7 @@ def initialize_database():
             user_id INTEGER, 
             ejercicio_id INTEGER, 
             FOREIGN KEY (user_id) REFERENCES usuarios(id), 
-            FOREIGN KEY (ejercicio_id) REFERENCES ejercicios(id)
+            FOREIGN KEY (ejercicio_id) REFERENCES datos(id)
         )
         """)
     conn.close()
@@ -80,7 +80,7 @@ def main(page: ft.Page):
                 current_user_id = user[0]
                 current_username = username
                 show_navigation_bar()
-                body_screen()
+                show_home()
                 return
             
             page.snack_bar.open = True
@@ -140,8 +140,28 @@ def main(page: ft.Page):
             destinations=[
                 ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Inicio"),
                 ft.NavigationDestination(icon=ft.icons.SPORTS_GYMNASTICS_ROUNDED, label="Ejercicios"),
+                ft.NavigationDestination(icon=ft.icons.STAR, label="Favoritos"),
                 ft.NavigationDestination(icon=ft.icons.ARROW_BACK, label="Atrás")
             ]
+        )
+        page.update()
+
+    def show_home(e=None):
+        def logout(e):
+            global current_user_id, current_username
+            current_user_id = None
+            current_username = None
+            page.navigation_bar = None
+            show_login()
+
+        page.clean()
+        logout_button = ft.ElevatedButton(text="Cerrar sesión", on_click=logout)
+        page.add(
+            ft.Image(src="icon.png", height=400),
+            ft.Container(padding=20),
+            ft.Text(f"Bienvenido, {current_username.capitalize()}!"),
+            ft.Container(padding=20),
+            logout_button
         )
         page.update()
 
@@ -152,7 +172,7 @@ def main(page: ft.Page):
 
         conn = sqlite3.connect(bd_path)
         cursor = conn.cursor()
-        cursor.execute("SELECT DISTINCT bodyPart FROM ejercicios")
+        cursor.execute("SELECT DISTINCT bodyPart FROM datos")
         body_parts = cursor.fetchall()
         conn.close()
 
@@ -169,99 +189,60 @@ def main(page: ft.Page):
                     ),
                     alignment=ft.alignment.center,
                 ),
-                on_click=lambda e, body_part=body_part: show_exercises(e, body_part[0]),
+                on_click=lambda e, body_part=body_part: equipment_screen(e, body_part[0]),
                 style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5))
             )
             bp.controls.append(button)
         page.update()
 
-    def show_exercises(e, body_part):
+    def equipment_screen(e, body_part):
+        history.append("body_part")
         page.clean()
-        history.append("body_screen")
-        
-        filter_dropdown_equipment = ft.Dropdown(
-            options=[],
-            on_change=lambda e: load_exercises(body_part, filter_dropdown_equipment.value, filter_dropdown_target.value),
-            label="Filter by Equipment",
-        )
-
-        filter_dropdown_target = ft.Dropdown(
-            options=[],
-            on_change=lambda e: load_exercises(body_part, filter_dropdown_equipment.value, filter_dropdown_target.value),
-            label="Filter by Target",
-        )
-
-        page.add(ft.Row([filter_dropdown_equipment, filter_dropdown_target]))
+        eq = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
+        page.add(eq)
 
         conn = sqlite3.connect(bd_path)
         cursor = conn.cursor()
-        cursor.execute("SELECT DISTINCT equipment FROM ejercicios WHERE bodyPart=?", (body_part,))
+        cursor.execute("SELECT DISTINCT equipment FROM datos WHERE bodyPart=?", (body_part,))
         equipments = cursor.fetchall()
-        cursor.execute("SELECT DISTINCT target FROM ejercicios WHERE bodyPart=?", (body_part,))
-        targets = cursor.fetchall()
         conn.close()
 
-        filter_dropdown_equipment.options = [ft.dropdown.Option(equip[0]) for equip in equipments]
-        filter_dropdown_equipment.options.insert(0, ft.dropdown.Option("Todos"))
+        for equip in equipments:
+            image_path = os.path.join(image_base_path, f"{body_part.lower()}.png")
+            button = ft.ElevatedButton(
+                content=ft.Container(
+                    content=ft.Column(
+                        [
+                            ft.Image(src=image_path, fit=ft.ImageFit.COVER, width=100, height=100),
+                            ft.Text(equip[0].upper())
+                        ],
+                        alignment=ft.alignment.center
+                    ),
+                    alignment=ft.alignment.center
+                ),
+                on_click=lambda e: exercise_screen(e, body_part, equip[0]),
+                style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5))
+            )
+            eq.controls.append(button)
 
-        filter_dropdown_target.options = [ft.dropdown.Option(target[0]) for target in targets]
-        filter_dropdown_target.options.insert(0, ft.dropdown.Option("Todos"))
+        page.update()
 
-        load_exercises(body_part)
-
-    def load_exercises(body_part, equipment_filter=None, target_filter=None):
+    def exercise_screen(e, body_part, equipment):
+        history.append("equipment")
         page.clean()
-        
-        filter_dropdown_equipment = ft.Dropdown(
-            options=[],
-            on_change=lambda e: load_exercises(body_part, e.control.value, target_filter),
-            label="Filter by Equipment",
-        )
-
-        filter_dropdown_target = ft.Dropdown(
-            options=[],
-            on_change=lambda e: load_exercises(body_part, equipment_filter, e.control.value),
-            label="Filter by Target",
-        )
 
         conn = sqlite3.connect(bd_path)
         cursor = conn.cursor()
-        cursor.execute("SELECT DISTINCT equipment FROM ejercicios WHERE bodyPart=?", (body_part,))
-        equipments = cursor.fetchall()
-        cursor.execute("SELECT DISTINCT target FROM ejercicios WHERE bodyPart=?", (body_part,))
-        targets = cursor.fetchall()
-        conn.close()
-
-        filter_dropdown_equipment.options = [ft.dropdown.Option(equip[0]) for equip in equipments]
-        filter_dropdown_equipment.options.insert(0, ft.dropdown.Option("Todos"))
-        filter_dropdown_equipment.value = equipment_filter if equipment_filter else "Todos"
-
-        filter_dropdown_target.options = [ft.dropdown.Option(target[0]) for target in targets]
-        filter_dropdown_target.options.insert(0, ft.dropdown.Option("Todos"))
-        filter_dropdown_target.value = target_filter if target_filter else "Todos"
-
-        page.add(ft.Row([filter_dropdown_equipment, filter_dropdown_target]))
-
-        conn = sqlite3.connect(bd_path)
-        cursor = conn.cursor()
-        query = "SELECT id, name, gifUrl, instructions, secondaryMuscles FROM ejercicios WHERE bodyPart=?"
-        params = [body_part]
-        if equipment_filter and equipment_filter != "Todos":
-            query += " AND equipment=?"
-            params.append(equipment_filter)
-        if target_filter and target_filter != "Todos":
-            query += " AND target=?"
-            params.append(target_filter)
-        cursor.execute(query, params)
+        cursor.execute("SELECT id, name, gifUrl, instructions FROM datos WHERE bodyPart=? AND equipment=?", (body_part, equipment))
         ejercicios = cursor.fetchall()
         conn.close()
 
         ejercicio_card = ft.Column(spacing=10, expand=True, scroll=ft.ScrollMode.ALWAYS)
 
-        def open_dlg(content, title="Instrucciones"):
+        def open_dlg(instructions):
             dlg = ft.AlertDialog(
-                title=ft.Text(title),
-                content=ft.Text(content),
+                title=ft.Text("Instrucciones"),
+                content=ft.Text(instructions),
                 actions=[ft.TextButton("Cerrar", on_click=lambda _: close_dlg(dlg))]
             )
             page.dialog = dlg
@@ -272,8 +253,36 @@ def main(page: ft.Page):
             dlg.open = False
             page.update()
 
+        def mark_favorite(e, ejercicio_id):
+            conn = sqlite3.connect(bd_path)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM favoritos WHERE user_id=? AND ejercicio_id=?", (current_user_id, ejercicio_id))
+            favorito = cursor.fetchone()
+            if favorito:
+                cursor.execute("DELETE FROM favoritos WHERE user_id=? AND ejercicio_id=?", (current_user_id, ejercicio_id))
+                conn.commit()
+                message = f"Ejercicio eliminado de favoritos"
+            else:
+                cursor.execute("INSERT INTO favoritos (user_id, ejercicio_id) VALUES (?, ?)", (current_user_id, ejercicio_id))
+                conn.commit()
+                message = f"Ejercicio añadido a favoritos"
+            conn.close()
+
+            page.snack_bar = ft.SnackBar(content=ft.Text(message))
+            page.snack_bar.open = True
+            page.update()
+            exercise_screen(e, body_part, equipment)
+
         for ejercicio in ejercicios:
-            ejercicio_id, name, gifUrl, instructions, secondaryMuscles = ejercicio
+            ejercicio_id, name, gifUrl, instructions = ejercicio
+
+            conn = sqlite3.connect(bd_path)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM favoritos WHERE user_id=? AND ejercicio_id=?", (current_user_id, ejercicio_id))
+            favorito = cursor.fetchone()
+            conn.close()
+
+            favorite_icon = ft.icons.STAR if favorito else ft.icons.STAR_OUTLINE_OUTLINED
 
             card_content = ft.Card(
                 content=ft.Container(
@@ -286,7 +295,12 @@ def main(page: ft.Page):
                             ft.Row(
                                 [
                                     ft.TextButton("Instrucciones", on_click=lambda e, inst=instructions: open_dlg(inst)),
-                                    ft.TextButton("Músculos Auxiliares", on_click=lambda e, secMuscles=secondaryMuscles: open_dlg(secMuscles, title="Músculos Auxiliares")),
+                                    ft.IconButton(
+                                        icon=favorite_icon,
+                                        icon_size=20,
+                                        tooltip="Favorito",
+                                        on_click=lambda e, ejercicio_id=ejercicio_id: mark_favorite(e, ejercicio_id)
+                                    )
                                 ],
                                 alignment=ft.MainAxisAlignment.END,
                             ),
@@ -299,18 +313,99 @@ def main(page: ft.Page):
             ejercicio_card.controls.append(card_content)
 
         page.add(ejercicio_card)
-        page.update()
+
+    def favorites_screen(e=None):
+        page.clean()
+
+        conn = sqlite3.connect(bd_path)
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT datos.name, datos.gifUrl, datos.instructions
+            FROM favoritos
+            JOIN datos ON favoritos.ejercicio_id = datos.id
+            WHERE favoritos.user_id = ?
+        """, (current_user_id,))
+        favoritos = cursor.fetchall()
+        conn.close()
+
+        favorites_list = ft.Column(spacing=10, expand=True, scroll=ft.ScrollMode.ALWAYS)
+
+        for fav in favoritos:
+            name, gifUrl, instructions = fav
+
+            def open_dlg(instructions):
+                dlg = ft.AlertDialog(
+                    title=ft.Text("Instrucciones"),
+                    content=ft.Text(instructions),
+                    actions=[ft.TextButton("Cerrar", on_click=lambda _: close_dlg(dlg))]
+                )
+                page.dialog = dlg
+                dlg.open = True
+                page.update()
+
+            def close_dlg(dlg):
+                dlg.open = False
+                page.update()
+
+            def remove_favorite(e, name):
+                conn = sqlite3.connect(bd_path)
+                cursor = conn.cursor()
+                cursor.execute("SELECT id FROM datos WHERE name=?", (name,))
+                ejercicio_id = cursor.fetchone()[0]
+                cursor.execute("DELETE FROM favoritos WHERE user_id=? AND ejercicio_id=?", (current_user_id, ejercicio_id))
+                conn.commit()
+                conn.close()
+
+                page.snack_bar = ft.SnackBar(content=ft.Text(f"{name} eliminado de favoritos"))
+                page.snack_bar.open = True
+                page.update()
+                favorites_screen()
+
+            card_content = ft.Card(
+                content=ft.Container(
+                    content=ft.Column(
+                        [
+                            ft.ListTile(
+                                title=ft.Text(name.upper()),
+                                subtitle=ft.Image(src=gifUrl),
+                            ),
+                            ft.Row(
+                                [
+                                    ft.TextButton("Instrucciones", on_click=lambda e, inst=instructions: open_dlg(inst)),
+                                    ft.IconButton(
+                                        icon=ft.icons.STAR,
+                                        icon_size=20,
+                                        tooltip="Eliminar de favoritos",
+                                        on_click=lambda e, name=name: remove_favorite(e, name)
+                                    )
+                                ],
+                                alignment=ft.MainAxisAlignment.END,
+                            ),
+                        ]
+                    ),
+                    padding=10,
+                )
+            )
+            favorites_list.controls.append(card_content)
+
+        page.add(favorites_list)
 
     def go_back(e):
         if history:
             last_action = history.pop()
-            if last_action == "body_screen":
+            if last_action == "equipment":
                 body_screen()
+            elif last_action == "body_part":
+                show_home()
 
     def nav_change(e):
         selected_index = e.control.selected_index
         if selected_index == 0:
+            show_home()
+        elif selected_index == 1:
             body_screen()
+        elif selected_index == 2:
+            favorites_screen()
         elif selected_index == 3:
             go_back(None)
 
