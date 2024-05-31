908e86bc1b5bebc9d7d192f4a8d386f7f8d2ff72
GinesRico <ginesrico89@gmail.com>
2024-05-31 09:20:00 +0200

    enlaces

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-image_base_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets"
-bd_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets/ejercicios.db"
+image_base_path = "https://raw.githubusercontent.com/GinesRico/Gym_App/b513245563949ff90c5921f625b87ad1f37d9f3e/assets/"
+#bd_path = "C:/Users/gines/OneDrive/Documentos/Gym_App/Gym_App/assets/ejercicios.db"
